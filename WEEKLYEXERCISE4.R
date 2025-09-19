c353241f61e55d4485fe6164f8dc1abf9389f3b4
Haedodam Kim <kimhaedodam@gmail.com>
2025-09-19 01:34:13 -0500

    library packages, read csv, writing questions.

--- WEEKLYEXERCISE4.R
+++ WEEKLYEXERCISE4.R
@@ -1,3 +1,17 @@
 library(usethis)
 git_default_branch()
-# main
+# default branch == main
+
+library(readr)
+library(tidyverse)
+library(ggplot2)
+
+olympics <- read_csv("Olympics.csv")
+
+# a. Calculate a new variable, called ‘total.medals’, which is the sum of gold, 
+# silver, and bronze, and add it to the Olympic dataset.
+
+# b. For each country, how many gold medals has it won?
+
+
+# c. For each year, how many total medals were given out?
