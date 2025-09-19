03150582e44273c4431b43a9f49103db5773a4ab
Haedodam Kim <kimhaedodam@gmail.com>
2025-09-19 01:53:31 -0500

    solve a

--- WEEKLYEXERCISE4.R
+++ WEEKLYEXERCISE4.R
@@ -10,6 +10,12 @@ olympics <- read_csv("Olympics.csv")
 
 # a. Which countries had the largest delegation of athletes in 1992? 
 # Create a tibble that contains only the variables country and athletes.
+largest_1992 <- olympics %>%
+  filter(year == 1992) %>%
+  select(country, athletes) %>%
+  slice_max(order_by = athletes, n = 1)
+
+largest_1992
 
 # For the following countries, plot the number of gold medals earned over time:
 # United States, France, Germany, Russia, and China.
